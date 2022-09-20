2d5d1ee8befb10860a2c72c52b939785830b04ff
Shireen T <shireentaha@shireens-mbp.lan>
2022-09-19 23:56:34 -0400

    mostly styling

---
+++ packages/frontend/pages/index.tsx
@@ -0,0 +1,12 @@
+import LandingPage from "./components/LandingPage";
+import type { NextPage } from 'next'
+import exp from "constants";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <LandingPage/>
+    </>
+  )
+}
+export default Home
--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -1,17 +1,35 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
       "./pages/**/*.{js,ts,jsx,tsx}",
       "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors:{
-      primary: '#065F46',
-      secondary: '#9BC1F9',
-      error:'#DC2626',
-      green:"rgba(34, 197, 94, .38)"
+    extend: {
+      colors:{
+        primary: '#065F46',
+        secondary: '#f47458',
+        alt_secondary: '#9bc1f9',
+        yellow: "#fae0b2",
+        error:'#DC2626',
+        alt_green: "#cdf2da",
+        green:"rgba(34, 197, 94, .38)",
+        light_green: "#e6ffef",
+        xlight_green: "#eefbf3",
+        off_white: "#fcfaf2",
+        light_yellow: "#fbebd7",
+        dark_yellow: "#f7cc7f"
+      },
+      fontFamily: {
+        sans: ["Urbanist", ...defaultTheme.fontFamily.serif],
+      }
     },
-    extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms')
+  ],
 }
