568d4f8391308d5323370f55624a38398a374a33
Shireen T <shireentaha@shireens-mbp.lan>
2022-09-24 22:37:49 -0400

    fundraisers page

--- packages/frontend/components/Card.tsx
+++ packages/frontend/components/Card.tsx
@@ -1,82 +1,91 @@
-import { useState } from 'react'
-import { Switch } from '@headlessui/react'
-import Image from 'next/image'
-
 interface CardInterface {
+  id: string
   title: string
   goal: number
+  owner: string
   fundsRaised: number
-  fundraiserTill?: string
   image: string
+  description?: string
+  endAt?: string
 }
 
 export default function Card({
+  id,
+  owner,
   title,
   goal,
   fundsRaised,
-  fundraiserTill,
+  endAt,
   image,
+  description
 }: CardInterface): JSX.Element {
   return (
-    <div className={`w-1/3 pb-10`}>
-      <a href="#">
-        <div className="max-w-sm rounded-lg border border-gray-200 bg-white shadow-md ">
-          <img className="rounded-t-lg" src={image} />
-          <svg className={'sr-only'}>{title}</svg>
-          <div className="p-5">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-black">
-              {title}
-            </h5>
-            <div className={'flex justify-between'}>
-              <p className="mb-3 font-normal text-gray-700 text-gray-400 ">
-                Goal
-              </p>
-              <p className=" mb-3 font-normal text-gray-700 text-gray-400 ">
-                {goal} $
-              </p>
-            </div>
+    <div key={id} className={`w-1/3 transition ease-in ease-out hover:scale-105 duration-5`}>
+      <div className="max-w-sm rounded-lg border border-gray-200 bg-white shadow-md">
+        <a href={`/fundraisers/${id}`}>
+          <img className="rounded-t-lg max-h-screen" src={image} />
+        </a>
+        <svg className={'sr-only'}>{title}</svg>
+        <div className="p-3">
+          <h1 className="mb-0.5 -mt-1.5 text-2xl tracking-tight text-black">
+            {title}
+          </h1>
+          <h5 className="mb-2 text-sm tracking-tight text-secondary">
+            {owner}
+          </h5>
 
-            <div className={'flex justify-between'}>
-              <p className="mb-3 font-normal text-gray-700 text-gray-400 ">
-                Funds Raised
-              </p>
-              <p className=" mb-3 font-normal text-gray-700 text-gray-400 ">
-                {fundsRaised} $
-              </p>
-            </div>
-
-            <div className={'flex justify-between'}>
-              <p className="mb-3 font-normal text-gray-700 text-gray-400 ">
-                Fundraiser Till
-              </p>
-              <p className=" mb-3 font-normal text-gray-700 text-gray-400 ">
-                {fundraiserTill}
-                {!fundraiserTill && '-'}
-              </p>
-            </div>
-
-            {/*<a*/}
-            {/*  href="#"*/}
-            {/*  className="inline-flex items-center rounded-lg bg-blue-700 py-2 px-3 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"*/}
-            {/*>*/}
-            {/*  Read more*/}
-            {/*  <svg*/}
-            {/*    aria-hidden="true"*/}
-            {/*    className="ml-2 -mr-1 h-4 w-4"*/}
-            {/*    fill="currentColor"*/}
-            {/*    viewBox="0 0 20 20"*/}
-            {/*    xmlns="http://www.w3.org/2000/svg"*/}
-            {/*  >*/}
-            {/*    <path*/}
-            {/*      fillRule="evenodd"*/}
-            {/*      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"*/}
-            {/*      clipRule="evenodd"*/}
-            {/*    ></path>*/}
-            {/*  </svg>*/}
-            {/*</a>*/}
+          <p className="mb-6 text-md tracking-tight text-gray-500">
+            {description}
+          </p>
+          <div className={'flex justify-between'}>
+            <p className="mb-1 text-sm text-gray-700 text-gray-400 ">
+              Goal
+            </p>
+            <p className=" mb-1 text-sm text-gray-700 text-gray-400 ">
+              ${goal}
+            </p>
           </div>
+
+          <div className={'flex justify-between'}>
+            <p className="mb-1 text-sm text-gray-700 text-gray-400 ">
+              Funds Raised
+            </p>
+            <p className=" mb-1 text-sm text-gray-700 text-gray-400 ">
+              ${fundsRaised}
+            </p>
+          </div>
+
+          <div className={'flex justify-between'}>
+            <p className="text-sm text-gray-700 text-gray-400 ">
+              Ends At
+            </p>
+            <p className="text-sm text-gray-700 text-gray-400 ">
+              {endAt}
+              {!endAt && '-'}
+            </p>
+          </div>
+
+          {/*<a*/}
+          {/*  href="#"*/}
+          {/*  className="inline-flex items-center rounded-lg bg-blue-700 py-2 px-3 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"*/}
+          {/*>*/}
+          {/*  Read more*/}
+          {/*  <svg*/}
+          {/*    aria-hidden="true"*/}
+          {/*    className="ml-2 -mr-1 h-4 w-4"*/}
+          {/*    fill="currentColor"*/}
+          {/*    viewBox="0 0 20 20"*/}
+          {/*    xmlns="http://www.w3.org/2000/svg"*/}
+          {/*  >*/}
+          {/*    <path*/}
+          {/*      fillRule="evenodd"*/}
+          {/*      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"*/}
+          {/*      clipRule="evenodd"*/}
+          {/*    ></path>*/}
+          {/*  </svg>*/}
+          {/*</a>*/}
         </div>
-      </a>
+      </div>
     </div>
   )
 }
--- packages/frontend/pages/fundraisers/index.tsx
+++ packages/frontend/pages/fundraisers/index.tsx
@@ -1,4 +1,13 @@
+import { gql } from '@apollo/client';
+import { InferGetServerSidePropsType, GetServerSideProps } from 'next';
+import { getToken } from 'next-auth/jwt';
+import { getSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { ParsedUrlQuery } from 'querystring';
+import Home from '..';
+import client from '../../apollo-client';
 import Card from '../../components/Card'
+import formatTimestamp from '../../utils/formatTimestamp';
 const fundraisers = [
   {
     id: 1,
@@ -56,23 +65,35 @@ const fundraisers = [
     image: '/placeholder-1.jpg',
   },
 ]
+type Props = InferGetServerSidePropsType<typeof getServerSideProps>;
 
-export default function Fundraisers() {
+
+
+export default function Fundraisers({campaigns, address, session}: Props) {
+  const router = useRouter();
+
+  if (!session) {
+    return <Home />
+  }
+
+
   return (
     <>
       <div className="mx-auto grid max-w-screen-xl py-8 px-4 pt-16 text-primary">
         <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-black md:text-5xl lg:text-4xl">
           Fundraisers
         </h1>
-        <div className={'flex max-w-screen-xl flex-wrap	'}>
-          {fundraisers.map((fundraiser) => (
+        <div className={'flex max-w-screen-xl'}>
+          {campaigns.map((fundraiser) => (
             <Card
-              key={fundraiser.id}
-              title={fundraiser.title}
+              id={fundraiser.id}
+              title={fundraiser.name}
+              owner={`${fundraiser.account.firstName} ${fundraiser.account.lastName}`}
+              description={fundraiser.description}
               goal={fundraiser.goal}
-              fundsRaised={fundraiser.fundsRaised}
-              fundraiserTill={fundraiser.fundraiserTill}
-              image={fundraiser.image}
+              fundsRaised={fundraiser.donated}
+              endAt={formatTimestamp(fundraiser.endAt)}
+              image={fundraiser.imageURL}
             />
           ))}
         </div>
@@ -80,3 +101,47 @@ export default function Fundraisers() {
     </>
   )
 }
+
+const CAMPAIGN_QUERY = gql`
+  query Campaigns($id: String)  {
+    campaigns {
+      id
+      name
+      description
+      goal
+      donated
+      imageURL
+      startAt
+      endAt
+      category {
+        id
+        name
+      }
+      account {
+        id
+        firstName
+        lastName
+        organization
+      }
+    }
+  }
+`
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+  const token = await getToken({ req: context.req });
+  const address = token?.sub ?? null;
+
+  const { data } = await client.query({
+    query: CAMPAIGN_QUERY
+  });
+
+  return {
+    props: {
+      campaigns: data.campaigns,
+      session: session,
+      address: address
+    },
+  };
+}
+
